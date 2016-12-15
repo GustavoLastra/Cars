eb55b4adfc12020bb828c7eee58bd58951b685cd
GustavoLastra <gustavo.lastrac@gmail.com>
2016-12-15 14:57:28 +0100

    tree modifications

--- Tree.js
+++ Tree.js
@@ -1,4 +1,4 @@
-function Tree (width, height, src, posX, posY,a) {
+function Tree (width, height, src, posX, posY) {
   this.width = width;
   this.height = height;
   this.speedX = 0;
@@ -22,28 +22,5 @@ function Tree (width, height, src, posX, posY,a) {
         this.gravitySpeed += this.gravity;
         this.x += this.speedX + this.gravitySpeed;
         this.y += this.speedY ;
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-        }
-    }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        //return crash;
     }
 }
